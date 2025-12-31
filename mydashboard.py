1b93c27ca55b4c612a360285cbc9a34b6548fe0d
korraweenit <korrawee.nit@gmail.com>
2026-01-01 00:44:53 +0700

    Fix dashboard link

--- mydashboard.py
+++ mydashboard.py
@@ -14,7 +14,7 @@ st.markdown("---")
 col1, col2 = st.columns(2)
 with col1:
     st.success("🇺🇸 **US Stocks**\n\nติดตามพอร์ตหุ้นอเมริกา เปรียบเทียบ SP500")
-    st.page_link("pages/1_US_Stocks.py", label="Go to US Stocks", icon="🇺🇸")
+    st.page_link("pages/1_US_stocks.py", label="Go to US Stocks", icon="🇺🇸")
 
 with col2:
     st.info("🇹🇭 **Mutual Funds**\n\nติดตามกองทุนรวมไทย (Coming Soon)")
