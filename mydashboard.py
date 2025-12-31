4a7bda2d203a3580150799152f33486af476fa05
korraweenit <korrawee.nit@gmail.com>
2026-01-01 00:53:57 +0700

    Fix icon emoji

--- mydashboard.py
+++ mydashboard.py
@@ -14,8 +14,8 @@ st.markdown("---")
 col1, col2 = st.columns(2)
 with col1:
     st.success("🇺🇸 **US Stocks**\n\nติดตามพอร์ตหุ้นอเมริกา เปรียบเทียบ SP500")
-    st.page_link("pages/1_US_stocks.py", label="Go to US Stocks", icon="🇺🇸")
+    st.page_link("pages/1_US_stocks.py", label="Go to US Stocks")
 
 with col2:
     st.info("🇹🇭 **Mutual Funds**\n\nติดตามกองทุนรวมไทย (Coming Soon)")
-    st.page_link("pages/2_Funds.py", label="Go to Funds", icon="🇹🇭")
+    st.page_link("pages/2_Funds.py", label="Go to Funds")
